4725f6279ba6c70af33a1a5ffb8edd0ee33cc1ac
Reuben Lifshay <rlifshay@gmail.com>
2017-02-15 17:05:24 -0800

    layers: Add subclasses for layers to inherit from based on caching strategy.

--- src/lcfs/layers/base.py
+++ src/lcfs/layers/base.py
@@ -1,5 +1,28 @@
-class Base:
-    '''Base class for cache layer implementations'''
+class BaseCache:
+    '''
+    Base class for all caching layers.
+
+    Cache layer implementations should not directly inherit from this class.
+    Instead, they should inherit one of the strategy based subclasses.
+    '''
 
     def __init__(self, config):
         self.config = config
+
+class FullCache(BaseCache):
+    '''
+    Base class for layers that only cache whole files.
+
+    The bottom layer of the layer stack must be an instance of this layer type
+    so that data is not lost due to only storing part of a file.
+    '''
+    pass
+
+class PartialCache(BaseCache):
+    '''
+    Base class for layers that cache sections of files or individual blocks.
+
+    Layers based on this cache are allowed to cache whole files, but they can
+    also choose to only cache parts of files or individual blocks.
+    '''
+    pass
