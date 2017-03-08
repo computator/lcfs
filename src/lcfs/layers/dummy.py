eaa044288a0a8df5b70ce05a0df3543a7d7693fd
Reuben Lifshay <rlifshay@gmail.com>
2017-03-07 22:26:04 -0800

    layers: add a dummy layer for testing.

---
+++ src/lcfs/layers/dummy.py
@@ -0,0 +1,7 @@
+from lcfs.layers.base import BaseLayer, BackingLayerType, CacheLayerType, FullDataStrategy
+
+class DummyBackingLayer(BaseLayer, BackingLayerType, FullDataStrategy):
+	pass
+
+class DummyCachingLayer(BaseLayer, CacheLayerType, FullDataStrategy):
+	pass
