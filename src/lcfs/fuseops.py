bdbc5b3926fadfe87a9233b8ae6a06d5b34d169c
Reuben Lifshay <rlifshay@gmail.com>
2017-03-11 05:16:51 -0800

    ops: use a map_err decorator to map OpErrors to FuseOSErrors.

--- src/lcfs/fuseops.py
+++ src/lcfs/fuseops.py
@@ -1,12 +1,19 @@
 from .operations import OpError
 from fuse import Operations, LoggingMixIn, FuseOSError
 
+def map_err(func):
+	def wrap(*args):
+		try:
+			return func(*args)
+		except OpError as e:
+			raise FuseOSError(e.errno)
+
+	return wrap
+
 class FuseOps(LoggingMixIn, Operations):
 	def __init__(self, op_mapper):
 		self.op_mapper = op_mapper
 
+	@map_err
 	def getattr(self, path, fh=None):
-		try:
-			return self.op_mapper.getattr(path, fh)
-		except OpError as e:
-			raise FuseOSError(e.errno)
+		return self.op_mapper.getattr(path, fh)
