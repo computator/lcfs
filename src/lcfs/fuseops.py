fb7a9e39c093f7da8e869c48eacdbc8f07146f6e
Reuben Lifshay <rlifshay@gmail.com>
2017-03-11 03:42:40 -0800

    ops: add beginning of first operation.

--- src/lcfs/fuseops.py
+++ src/lcfs/fuseops.py
@@ -1,5 +1,12 @@
+from .operations import OpError
 from fuse import Operations, LoggingMixIn, FuseOSError
 
 class FuseOps(LoggingMixIn, Operations):
 	def __init__(self, op_mapper):
 		self.op_mapper = op_mapper
+
+	def getattr(self, path, fh=None):
+		try:
+			return self.op_mapper.getattr(path, fh)
+		except OpError as e:
+			raise FuseOSError(e.errno)
